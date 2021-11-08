dd51a670c5e1baac3aa5b63d2726df2e66a0d496
danggai <donggi9313@naver.com>
2021-11-08 11:04:15 +0900

    - API 헤더 추가

--- app/src/main/java/danggai/app/resinwidget/data/api/ApiInterface.kt
+++ app/src/main/java/danggai/app/resinwidget/data/api/ApiInterface.kt
@@ -11,6 +11,7 @@ interface ApiInterface {
     @Headers(
         "x-rpc-client_type: 4",
         "x-rpc-app_version: 1.5.0",
+        "x-rpc-language: ko-kr",
     )
     @GET("/game_record/genshin/api/dailyNote?server=os_asia")
     fun dailyNote(@Query("role_id") uid: String, @Header("Cookie") cookie: String, @Header("DS") ds: String): Observable<Response<ResDailyNote.Data>>
@@ -18,9 +19,10 @@ interface ApiInterface {
     @Headers(
         "x-rpc-client_type: 4",
         "x-rpc-app_version: 1.5.0",
+        "x-rpc-language: ko-kr",
     )
     @POST("/game_record/card/wapi/changeDataSwitch")
     fun changeDataSwitch(@Query("game_id") gameId: Int, @Query("switch_id") switchId: Int, @Query("is_public") isPublic: Boolean, @Header("Cookie") cookie: String, @Header("DS") ds: String): Observable<Response<ResChangeDataSwitch.Data>>
-    /* 원신 기준, game_id:2,switch_id:3 */
+    /* 원신 기준, game_id:2,switch_id:1~4 */
 
 }
