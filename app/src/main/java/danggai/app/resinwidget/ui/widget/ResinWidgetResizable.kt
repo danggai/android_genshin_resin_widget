201ab6c94b7f36696c34e30e38ecc2e7ef397671
danggai <donggi9313@naver.com>
2021-11-23 10:57:33 +0900

    - 자유로운 위젯 크기 기능 구현

--- app/src/main/java/danggai/app/resinwidget/ui/widget/ResinWidgetResizable.kt
+++ app/src/main/java/danggai/app/resinwidget/ui/widget/ResinWidgetResizable.kt
@@ -8,6 +8,7 @@ import android.content.Context
 import android.content.Intent
 import android.view.View
 import android.widget.RemoteViews
+import android.widget.Toast
 import danggai.app.resinwidget.Constant
 import danggai.app.resinwidget.R
 import danggai.app.resinwidget.ui.main.MainActivity
@@ -16,7 +17,7 @@ import danggai.app.resinwidget.util.PreferenceManager
 import danggai.app.resinwidget.util.log
 
 
-class ResinWidget : AppWidgetProvider() {
+class ResinWidgetResizable : AppWidgetProvider() {
 
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
         super.onUpdate(context, appWidgetManager, appWidgetIds)
@@ -37,7 +38,7 @@ class ResinWidget : AppWidgetProvider() {
             Constant.ACTION_APPWIDGET_UPDATE -> {
                 log.e("ACTION_APPWIDGET_UPDATE")
 
-                val thisWidget = ComponentName(context!!, ResinWidget::class.java)
+                val thisWidget = ComponentName(context!!, ResinWidgetResizable::class.java)
                 val appWidgetManager = AppWidgetManager.getInstance(context)
                 val appWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget)
 
@@ -52,10 +53,10 @@ class ResinWidget : AppWidgetProvider() {
 
                     context?.let { it ->
 
-                        val _intent = Intent(it, ResinWidget::class.java)
+                        val _intent = Intent(it, ResinWidgetResizable::class.java)
                         _intent.action = AppWidgetManager.ACTION_APPWIDGET_UPDATE
 
-                        val ids = AppWidgetManager.getInstance(it.applicationContext).getAppWidgetIds(ComponentName(it.applicationContext, ResinWidget::class.java))
+                        val ids = AppWidgetManager.getInstance(it.applicationContext).getAppWidgetIds(ComponentName(it.applicationContext, ResinWidgetResizable::class.java))
 
                         _intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, ids)
                         it.sendBroadcast(_intent)
@@ -72,21 +73,22 @@ class ResinWidget : AppWidgetProvider() {
         super.onEnabled(context)
         log.e()
         context?.let {
+            Toast.makeText(context, "현재 레진 갯수가 보이지 않는다면, 우상단 새로고침 버튼을 눌러주세요.", Toast.LENGTH_SHORT).show()
             CommonFunction.startUniquePeriodicRefreshWorker(context)
         }
     }
 
     private fun addViews(context: Context?): RemoteViews {
         log.e()
-        val views = RemoteViews(context!!.packageName, R.layout.widget_resin_fixed)
+        val views = RemoteViews(context!!.packageName, R.layout.widget_resin_resizable)
 
-        val intentUpdate = Intent(context, ResinWidget::class.java)
+        val intentUpdate = Intent(context, ResinWidgetResizable::class.java)
         intentUpdate.action = AppWidgetManager.ACTION_APPWIDGET_UPDATE
         intentUpdate.putExtra(Constant.REFRESH_DATA, true)
         intentUpdate.putExtra(Constant.REFRESH_UI, true)
 
         val manager: AppWidgetManager = AppWidgetManager.getInstance(context)
-        val awId = manager.getAppWidgetIds(ComponentName(context.applicationContext, ResinWidget::class.java))
+        val awId = manager.getAppWidgetIds(ComponentName(context.applicationContext, ResinWidgetResizable::class.java))
 
         views.setOnClickPendingIntent(R.id.ll_sync, PendingIntent.getBroadcast(context, 0, intentUpdate, PendingIntent.FLAG_UPDATE_CURRENT))
 
@@ -139,7 +141,7 @@ class ResinWidget : AppWidgetProvider() {
 
         appWidgetIds.forEach { appWidgetId ->
             log.e()
-            val view = RemoteViews(context.packageName, R.layout.widget_resin_fixed)
+            val view = RemoteViews(context.packageName, R.layout.widget_resin_resizable)
 
             view.setViewVisibility(R.id.progress1, View.VISIBLE)
             view.setViewVisibility(R.id.iv_resin, View.GONE)
