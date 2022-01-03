31fafc308a5c2cb08cfbabeaad0bca44dab652ab
danggai <donggi9313@naver.com>
2022-01-03 17:04:12 +0900

    - 자세한 정보가 담긴 위젯 UI 구현

--- app/src/main/java/danggai/app/resinwidget/data/local/DailyNote.kt
+++ app/src/main/java/danggai/app/resinwidget/data/local/DailyNote.kt
@@ -4,14 +4,21 @@ data class DailyNote(
     val current_resin: Int,
     val max_resin: Int,
     val resin_recovery_time: String?,
+
+    val finished_task_num: Int,
+    val total_task_num: Int,
+    val is_extra_task_reward_received: Boolean, // 일일임무
+
+    val remain_resin_discount_num: Int,
+    val resin_discount_num_limit: Int,          // 주간 보스 할인
+
     val current_expedition_num: Int,
     val max_expedition_num: Int,
     val expeditions: List<GameRoleExpedition>?
 ) {
-
     data class GameRoleExpedition (
         val avatar_side_icon: String,
         val status: String,
         val remained_time: String
-        )
+    )
 }
