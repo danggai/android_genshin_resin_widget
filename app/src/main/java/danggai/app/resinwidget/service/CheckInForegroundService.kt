19ccd1eeec91aef8b28427aa18b2a6db0c20f560
danggai <donggi9313@naver.com>
2022-01-20 16:03:40 +0900

    - 자동 출석체크용 포그라운드 서비스 구현

---
+++ app/src/main/java/danggai/app/resinwidget/service/CheckInForegroundService.kt
@@ -0,0 +1,181 @@
+package danggai.app.resinwidget.service
+
+import android.app.*
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Build
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import danggai.app.resinwidget.Constant
+import danggai.app.resinwidget.R
+import danggai.app.resinwidget.data.api.ApiInterface
+import danggai.app.resinwidget.data.api.ApiRepository
+import danggai.app.resinwidget.ui.main.MainActivity
+import danggai.app.resinwidget.util.CommonFunction
+import danggai.app.resinwidget.util.PreferenceManager
+import danggai.app.resinwidget.util.log
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.PublishSubject
+import org.koin.android.ext.android.inject
+import org.koin.core.context.GlobalContext
+
+class CheckInForegroundService() : Service() {
+
+
+    private val rxApiCheckIn: PublishSubject<Boolean> = PublishSubject.create()
+    private val compositeDisposable = CompositeDisposable()
+    private fun Disposable.addCompositeDisposable() {
+        compositeDisposable.add(this)
+    }
+
+    private val api: ApiRepository by inject()
+
+    init {
+        initRx()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        rxApiCheckIn.onNext(true)
+    }
+
+    private fun initRx() {
+        rxApiCheckIn
+            .observeOn(Schedulers.io())
+            .filter { it }
+            .switchMap {
+                val cookie = PreferenceManager.getStringCookie(applicationContext)
+
+                api.checkIn(Constant.SERVER_OS_ASIA, cookie)
+            }
+            .subscribe ({ res ->
+                log.e(res)
+                when (res.meta.code) {
+                    Constant.META_CODE_SUCCESS -> {
+                        log.e()
+                        when (res.data.retcode) {
+                            Constant.RETCODE_SUCCESS -> {
+                                log.e()
+                                if (PreferenceManager.getBooleanNotiCheckInSuccess(applicationContext)) {
+                                    log.e()
+                                    sendNoti(Constant.NOTI_TYPE_CHECK_IN_SUCCESS)
+                                    CommonFunction.startUniquePeriodicCheckInWorker(applicationContext, false)
+                                }
+                            }
+                            Constant.RETCODE_ERROR_CLAIMED_DAILY_REWARD,
+                            Constant.RETCODE_ERROR_CHECKED_INTO_HOYOLAB, -> {
+                                log.e()
+                                if (PreferenceManager.getBooleanNotiCheckInSuccess(applicationContext)) {
+                                    log.e()
+                                    sendNoti(Constant.NOTI_TYPE_CHECK_IN_ALREADY)
+                                    CommonFunction.startUniquePeriodicCheckInWorker(applicationContext, false)
+                                }
+                            }
+                            else -> {
+                                log.e()
+                                if (PreferenceManager.getBooleanNotiCheckInFailed(applicationContext)) {
+                                    log.e()
+                                    sendNoti(Constant.NOTI_TYPE_CHECK_IN_FAILED)
+                                }
+                                CommonFunction.sendCrashlyticsApiLog(Constant.API_NAME_CHECK_IN, res.meta.code, res.data.retcode)
+                                CommonFunction.startUniquePeriodicCheckInWorker(applicationContext, true)
+                            }
+                        }
+                    }
+                    else -> {
+                        log.e()
+                        if (PreferenceManager.getBooleanNotiCheckInFailed(applicationContext)) {
+                            log.e()
+                            sendNoti(Constant.NOTI_TYPE_CHECK_IN_FAILED)
+                        }
+                        CommonFunction.sendCrashlyticsApiLog(Constant.API_NAME_CHECK_IN, res.meta.code, null)
+                        applicationContext.sendBroadcast(CommonFunction.getIntentAppWidgetUiUpdate())
+                        CommonFunction.startUniquePeriodicCheckInWorker(applicationContext, true)
+                    }
+                }
+
+                stopForeground(true)
+            }, {
+                it.message?.let { msg ->
+                    applicationContext.sendBroadcast(CommonFunction.getIntentAppWidgetUiUpdate())
+
+                    stopForeground(true)
+                    log.e(msg)
+                }
+            })
+            .addCompositeDisposable()
+    }
+
+    private fun sendNoti(id: Int) {
+        log.e()
+
+        val title = applicationContext.getString(R.string.push_checkin_title)
+        val msg = when (id) {
+            Constant.NOTI_TYPE_CHECK_IN_SUCCESS -> applicationContext.getString(R.string.push_msg_checkin_success)
+            Constant.NOTI_TYPE_CHECK_IN_ALREADY -> applicationContext.getString(R.string.push_msg_checkin_already)
+            Constant.NOTI_TYPE_CHECK_IN_FAILED -> applicationContext.getString(R.string.push_msg_checkin_failed)
+            else -> ""
+        }
+        CommonFunction.sendNotification(Constant.NOTI_TYPE_CHECK_IN_SUCCESS, applicationContext, title, msg)
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        log.e()
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        generateForegroundNotification()
+        return START_NOT_STICKY
+    }
+
+    override fun onDestroy() {
+        stopForeground(true)
+    }
+
+    private var iconNotification: Bitmap? = null
+    private var notification: Notification? = null
+    var mNotificationManager: NotificationManager? = null
+    private val mNotificationId = 123
+
+    private fun generateForegroundNotification() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val intentMainLanding = Intent(this, MainActivity::class.java)
+            val pendingIntent =
+                PendingIntent.getActivity(this, 0, intentMainLanding, 0)
+            iconNotification = BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher)
+            if (mNotificationManager == null) {
+                mNotificationManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                assert(mNotificationManager != null)
+                val notificationChannel =
+                    NotificationChannel(Constant.PUSH_CHANNEL_CHECK_IN_PROGRESS_NOTI_ID, Constant.PUSH_CHANNEL_CHECK_IN_PROGRESS_NOTI_NAME,
+                        NotificationManager.IMPORTANCE_MIN)
+                notificationChannel.enableLights(false)
+                notificationChannel.lockscreenVisibility = Notification.VISIBILITY_SECRET
+                mNotificationManager?.createNotificationChannel(notificationChannel)
+            }
+            val builder = NotificationCompat.Builder(this, Constant.PUSH_CHANNEL_CHECK_IN_PROGRESS_NOTI_ID)
+
+            builder.setContentTitle(applicationContext.getString(R.string.foreground_genshin_check_in_progress))
+                .setTicker(applicationContext.getString(R.string.foreground_genshin_check_in_progress))
+                .setSmallIcon(R.drawable.resin)
+                .setPriority(NotificationCompat.PRIORITY_LOW)
+                .setWhen(0)
+                .setOnlyAlertOnce(true)
+                .setContentIntent(pendingIntent)
+                .setOngoing(true)
+            if (iconNotification != null) {
+                builder.setLargeIcon(Bitmap.createScaledBitmap(iconNotification!!, 128, 128, false))
+            }
+            notification = builder.build()
+            startForeground(mNotificationId, notification)
+        }
+
+    }
+}
--- app/src/main/java/danggai/app/resinwidget/worker/CheckInWorker.kt
+++ app/src/main/java/danggai/app/resinwidget/worker/CheckInWorker.kt
@@ -1,11 +1,13 @@
 package danggai.app.resinwidget.worker
 
 import android.content.Context
+import android.content.Intent
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import danggai.app.resinwidget.Constant
 import danggai.app.resinwidget.R
 import danggai.app.resinwidget.data.api.ApiRepository
+import danggai.app.resinwidget.service.CheckInForegroundService
 import danggai.app.resinwidget.util.CommonFunction
 import danggai.app.resinwidget.util.PreferenceManager
 import danggai.app.resinwidget.util.log
@@ -19,106 +21,11 @@ import java.net.UnknownHostException
 class CheckInWorker (val context: Context, workerParams: WorkerParameters, private val api: ApiRepository) :
     Worker(context, workerParams) {
 
-    companion object {
-    }
-
-    private val rxApiCheckIn: PublishSubject<Boolean> = PublishSubject.create()
-    private val compositeDisposable = CompositeDisposable()
-    private fun Disposable.addCompositeDisposable() {
-        compositeDisposable.add(this)
-    }
-
-    init {
-        initRx()
-    }
-
-    private fun initRx() {
-        rxApiCheckIn
-            .observeOn(Schedulers.io())
-            .filter { it }
-            .switchMap {
-                val cookie = PreferenceManager.getStringCookie(applicationContext)
-
-                api.checkIn(Constant.SERVER_OS_ASIA, cookie)
-            }
-            .subscribe ({ res ->
-                log.e(res)
-                when (res.meta.code) {
-                    Constant.META_CODE_SUCCESS -> {
-                        log.e()
-                        when (res.data.retcode) {
-                            Constant.RETCODE_SUCCESS -> {
-                                log.e()
-                                if (PreferenceManager.getBooleanNotiCheckInSuccess(context)) {
-                                    log.e()
-                                    sendNoti(Constant.NOTI_TYPE_CHECK_IN_SUCCESS)
-                                }
-                                CommonFunction.startUniquePeriodicCheckInWorker(context, false)
-                            }
-                            Constant.RETCODE_ERROR_CLAIMED_DAILY_REWARD,
-                            Constant.RETCODE_ERROR_CHECKED_INTO_HOYOLAB, -> {
-                                log.e()
-                                if (PreferenceManager.getBooleanNotiCheckInSuccess(context)) {
-                                    log.e()
-                                    sendNoti(Constant.NOTI_TYPE_CHECK_IN_ALREADY)
-                                }
-                                CommonFunction.startUniquePeriodicCheckInWorker(context, false)
-                            }
-                            else -> {
-                                log.e()
-                                if (PreferenceManager.getBooleanNotiCheckInFailed(context)) {
-                                    log.e()
-                                    sendNoti(Constant.NOTI_TYPE_CHECK_IN_FAILED)
-                                }
-                                CommonFunction.sendCrashlyticsApiLog(Constant.API_NAME_CHECK_IN, res.meta.code, res.data.retcode)
-                                CommonFunction.startUniquePeriodicCheckInWorker(context, true)
-                            }
-                        }
-                    }
-                    else -> {
-                        log.e()
-                        if (PreferenceManager.getBooleanNotiCheckInFailed(context)) {
-                            log.e()
-                            sendNoti(Constant.NOTI_TYPE_CHECK_IN_FAILED)
-                        }
-                        CommonFunction.sendCrashlyticsApiLog(Constant.API_NAME_CHECK_IN, res.meta.code, null)
-                        context.sendBroadcast(CommonFunction.getIntentAppWidgetUiUpdate())
-                        CommonFunction.startUniquePeriodicCheckInWorker(context, true)
-                    }
-                }
-            }, {
-                it.message?.let { msg ->
-                    initRx()
-                    context.sendBroadcast(CommonFunction.getIntentAppWidgetUiUpdate())
-                    log.e(msg)
-                }
-            })
-            .addCompositeDisposable()
-    }
-
-    private fun sendNoti(id: Int) {
-        log.e()
-
-        if (inputData.getBoolean(Constant.ARG_IS_ONE_TIME, false)) {
-            log.e()
-            return
-        }
-
-        val title = applicationContext.getString(R.string.push_checkin_title)
-        val msg = when (id) {
-            Constant.NOTI_TYPE_CHECK_IN_SUCCESS -> applicationContext.getString(R.string.push_msg_checkin_success)
-            Constant.NOTI_TYPE_CHECK_IN_ALREADY -> applicationContext.getString(R.string.push_msg_checkin_already)
-            Constant.NOTI_TYPE_CHECK_IN_FAILED -> applicationContext.getString(R.string.push_msg_checkin_failed)
-            else -> ""
-        }
-        CommonFunction.sendNotification(Constant.NOTI_TYPE_CHECK_IN_SUCCESS, applicationContext, title, msg)
-    }
-
     override fun doWork(): Result {
         log.e()
 
         try {
-            rxApiCheckIn.onNext(true)
+            context.startService(Intent(context, CheckInForegroundService::class.java))
 
             log.e()
             return Result.success()
