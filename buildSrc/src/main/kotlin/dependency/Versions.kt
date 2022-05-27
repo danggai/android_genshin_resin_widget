990c66c9fac3d983ba22a8fb4548b2926399dd8b
danggai <donggi9313@naver.com>
2022-05-27 16:24:20 +0900

    - Kotlin DSL 마이그레이션 진행 중

---
+++ buildSrc/src/main/kotlin/dependency/Versions.kt
@@ -0,0 +1,60 @@
+package dependency
+
+object Versions {
+    const val minSdk                 = 23
+    const val compileSdk             = 32
+
+    const val versionCode            = 31
+    const val versionName            = "1.1.5"
+
+    // gradle plugins
+    const val buildGradleVersion     = "7.1.0"
+    const val kotlinGradleVersion    = "1.6.20"
+    const val googleServiceVersion   = "4.3.10"
+    const val crashlyticsGradleVersion = "2.7.1"
+    const val gsonVersion            = "2.8.6"
+
+    // android support library
+    const val coreVersion            = "1.6.0"
+    const val appCompatVersion       = "1.3.1"
+    const val activityKtxVersion     = "1.3.1"
+    const val fragmentKtxVersion     = "1.4.0"
+    const val materialVersion        = "1.4.0"
+    const val constraintVersion      = "2.1.0"
+    const val lifecycleVersion       = "2.4.1"
+
+    // coroutine
+    const val coroutineVersion       = "1.6.0"
+
+    // rx
+    const val rxAndroidVersion       = "2.1.1"
+    const val rxJavaVersion          = "2.2.17"
+    const val rxKotlinVersion        = "2.4.0"
+
+    // di
+    const val hiltVersion            = "2.41"
+    const val hiltWorkVersion        = "1.0.0"
+
+    // network
+    const val okhttpVersion          = "4.9.0"
+    const val retrofitVersion        = "2.6.0"
+    const val sandwichVersion        = "1.2.3"
+    const val glideVersion           = "4.13.0"
+
+    // google ads
+    const val googleAdsVersion       = "20.4.0"
+
+    // worker
+    const val workerVersion          = "2.7.1"
+
+    // firebase
+    const val firebaseVersion        = "28.4.1"
+    const val firebaseAnalyticsVersion = "20.1.0"
+    const val firebaseCrashlyticsVersion = "18.2.9"
+
+
+    // test
+    const val junitVersion           = "4.+"
+    const val junitExtVersion        = "1.1.3"
+    const val EspressoVersion        = "3.4.0"
+}
