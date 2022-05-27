864e67c29991fb127d7065f5abac413aeafe54bd
danggai <donggi9313@naver.com>
2022-05-27 16:24:20 +0900

    - Kotlin DSL 마이그레이션 진행 중

---
+++ buildSrc/src/main/kotlin/dependency/Dependencies.kt
@@ -0,0 +1,8 @@
+package dependency
+
+object Dependencies {
+//    object Support {
+//        const val annotations = "androidx.annotation:annotation:${Versions.support}"
+//        const val app_compat = "androidx.appcompat:appcompat:${Versions.app_compat}"
+//    }
+}
