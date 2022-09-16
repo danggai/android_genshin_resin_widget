3a1a6314917e08f257e620ce6750c64bcfaa0895
danggai <donggi9313@naver.com>
2022-09-16 11:21:37 +0900

    - 3.1.1 버전이름/버전코드 업그레이드

--- buildSrc/src/main/kotlin/dependency/Versions.kt
+++ buildSrc/src/main/kotlin/dependency/Versions.kt
@@ -4,8 +4,8 @@ object Versions {
     const val minSdk                 = 23
     const val compileSdk             = 32
 
-    const val versionCode            = 34
-    const val versionName            = "3.1.0"
+    const val versionCode            = 35
+    const val versionName            = "3.1.1"
 
     // gradle plugins
     const val buildGradleVersion     = "7.2.1"
