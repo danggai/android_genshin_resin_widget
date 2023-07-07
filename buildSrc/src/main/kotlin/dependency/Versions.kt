9178950e3453039d67078c24f1ece76631864376
danggai <donggi9313@naver.com>
2023-07-08 02:27:55 +0900

    - version code updated: 3.8.0 / 55

--- buildSrc/src/main/kotlin/dependency/Versions.kt
+++ buildSrc/src/main/kotlin/dependency/Versions.kt
@@ -4,8 +4,8 @@ object Versions {
     const val minSdk                 = 23
     const val compileSdk             = 32
 
-    const val versionCode            = 54
-    const val versionName            = "3.7.2"
+    const val versionCode            = 55
+    const val versionName            = "3.8.0"
 
     // gradle plugins
     const val buildGradleVersion     = "7.2.1"
