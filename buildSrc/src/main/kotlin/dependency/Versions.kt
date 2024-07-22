4bfb666f1972ed2e41bea57b8aaff3d64773d9eb
danggai <donggi9313@naver.com>
2024-07-22 17:18:51 +0900

    - version code updated: 4.8.1 / 72 - 패치노트 작성

--- buildSrc/src/main/kotlin/dependency/Versions.kt
+++ buildSrc/src/main/kotlin/dependency/Versions.kt
@@ -4,8 +4,8 @@ object Versions {
     const val minSdk = 23
     const val compileSdk = 34
 
-    const val versionCode = 71
-    const val versionName = "4.8.0"
+    const val versionCode = 72
+    const val versionName = "4.8.1"
 
     // gradle plugins
     const val buildGradleVersion = "7.4.2"
