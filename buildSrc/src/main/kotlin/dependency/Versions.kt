454dfb1090b4994696b62a0df41a7fedf8abd96c
danggai <donggi9313@naver.com>
2024-11-20 00:28:48 +0900

    - version code updated: 75

--- buildSrc/src/main/kotlin/dependency/Versions.kt
+++ buildSrc/src/main/kotlin/dependency/Versions.kt
@@ -4,7 +4,7 @@ object Versions {
     const val minSdk = 23
     const val compileSdk = 34
 
-    const val versionCode = 74
+    const val versionCode = 75
     const val versionName = "5.0.0"
 
     // gradle plugins
