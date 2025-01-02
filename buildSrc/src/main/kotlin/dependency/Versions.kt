ac25b6b4fce8c0e03daa9964205a832a42d95fba
danggai <donggi9313@naver.com>
2025-01-02 10:51:43 +0900

    !HOTFIX - 앱 버전 (5.3.0) 수정

--- buildSrc/src/main/kotlin/dependency/Versions.kt
+++ buildSrc/src/main/kotlin/dependency/Versions.kt
@@ -4,6 +4,6 @@ object Versions {
     const val minSdk = 23
     const val compileSdk = 34
 
-    const val versionCode = 75
-    const val versionName = "5.0.0"
+    const val versionCode = 76
+    const val versionName = "5.3.0"
 }
