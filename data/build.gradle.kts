7a101ea296b8fac0c08bfb419339fecb22aad5fe
danggai <donggi9313@naver.com>
2025-01-22 19:43:41 +0900

    #66: 자바 버전 업데이트

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -34,8 +34,8 @@ android {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 dependencies {
@@ -79,11 +79,11 @@ dependencies {
 
 android {
     compileOptions {
-        sourceCompatibility(JavaVersion.VERSION_1_8)
-        targetCompatibility(JavaVersion.VERSION_1_8)
+        sourceCompatibility(JavaVersion.VERSION_17)
+        targetCompatibility(JavaVersion.VERSION_17)
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     namespace = "danggai.data"
 }
