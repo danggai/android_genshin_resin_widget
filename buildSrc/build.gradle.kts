1cbd7d1868e0fecf720b507ce61b792b5b5fb3bf
danggai <donggi9313@naver.com>
2025-01-23 16:54:36 +0900

    #66: 타겟 SDK 수정 및 관련 gradle plugin 버전 수정

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.20")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
-    implementation("com.android.tools.build:gradle:8.4.0")
-    implementation("com.android.tools.build:gradle-api:8.4.0")
+    implementation("com.android.tools.build:gradle:8.4.2")
+    implementation("com.android.tools.build:gradle-api:8.4.2")
     implementation("com.squareup:javapoet:1.13.0")
 }
--- buildSrc/src/main/kotlin/dependency/Versions.kt
+++ buildSrc/src/main/kotlin/dependency/Versions.kt
@@ -2,8 +2,8 @@ package dependency
 
 object Versions {
     const val minSdk = 23
-    const val compileSdk = 34
-    const val compileSdkString = "android-34"
+    const val compileSdk = 35
+    const val compileSdkString = "android-35"
 
     const val versionCode = 80
     const val versionName = "5.3.1"
