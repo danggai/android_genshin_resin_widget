2f70f4477fef0eea9f665a0fe11e17d99bb37a32
danggai <donggi9313@naver.com>
2025-08-04 12:13:06 +0900

    - 6.0: 버전 코드, 이름 업데이트 및 패치 노트 반영

--- buildSrc/src/main/kotlin/dependency/Versions.kt
+++ buildSrc/src/main/kotlin/dependency/Versions.kt
@@ -5,6 +5,6 @@ object Versions {
     const val compileSdk = 35
     const val compileSdkString = "android-35"
 
-    const val versionCode = 82
-    const val versionName = "5.6.0"
+    const val versionCode = 83
+    const val versionName = "6.0.0"
 }
