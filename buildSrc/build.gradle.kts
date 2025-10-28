36f6a4088c78c9ce8b0ae55b55bfd10b87ae9406
danggai <donggi9313@naver.com>
2025-10-28 21:00:32 +0900

    - 코틀린 버전 1.9.21로 마이그레이션

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,8 +8,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.20")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.21")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
     implementation("com.android.tools.build:gradle:8.4.2")
     implementation("com.android.tools.build:gradle-api:8.4.2")
     implementation("com.squareup:javapoet:1.13.0")
