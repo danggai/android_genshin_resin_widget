62f45fc5568a91d6971434183d1aed7afabb9b49
danggai <donggi9313@naver.com>
2025-10-29 18:33:05 +0900

    - 상/하단 시스템 영역 침범하지 않도록 수정

--- presentation/src/main/java/danggai/app/presentation/core/BaseActivity.kt
+++ presentation/src/main/java/danggai/app/presentation/core/BaseActivity.kt
@@ -1,12 +1,21 @@
 package danggai.app.presentation.core
 
+import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 
-open class BaseActivity: AppCompatActivity() {
+open class BaseActivity : AppCompatActivity() {
 
     private val compositeDisposable = CompositeDisposable()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // 상태바 투명화
+        window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+    }
 
     fun Disposable.addDisposableExt() {
         compositeDisposable.add(this)
